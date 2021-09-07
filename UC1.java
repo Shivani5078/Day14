6dd6436bf4d67d6e9590ff3a9f791fbbbacbb2e6
Shivani5078 <88040014+Shivani5078@users.noreply.github.com>
2021-09-07 16:11:04 +0530

    Add: Insert stack element

---
+++ UC1.java
@@ -0,0 +1,17 @@
+package datastructur.com;
+import java.util.LinkedList;
+
+public class UC1 {
+	public static void main(String args[]) {
+// Enetr element in stack
+		
+		LinkedList<Integer>number = new LinkedList<Integer>();
+		number.push(70);
+		number.push(30);
+		number.push(56);
+		
+	    System.out.println(number);
+	    
+		}
+	}
+
