607ca8f69be9b062c7712c2095d8f119024fbaf0
Shivani5078 <88040014+Shivani5078@users.noreply.github.com>
2021-09-07 14:59:09 +0530

    add:intial commit

---
+++ Stack.java
@@ -0,0 +1,24 @@
+package company.com;
+
+import java.util.LinkedList;
+/**
+@purpose:stack opration(push) 
+*@author:shivani
+public class Stack {
+
+	 * @return: No return value
+	 */
+	
+	public static void main(String args[]) {
+*/ main method
+
+	LinkedList<Integer>number = new LinkedList<Integer>();
+  */linkedlist object create
+	number.push(70); //  method call
+	number.push(30);
+	number.push(56);
+	
+    System.out.println(number);
+    
+	}
+}
